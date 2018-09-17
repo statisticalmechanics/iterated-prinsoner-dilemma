76f9d7f5db5221337f12cc2e87809a5c84710c05
Kai Zhang <kai.zhang.statmech@gmail.com>
2018-09-16 21:00:24 -0400

    app

--- controller.py
+++ controller.py
@@ -54,8 +54,8 @@ def tour():
 
 if __name__ == '__main__':
     app.run(debug=True)
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    #port = int(os.environ.get("PORT", 5000))
+    #app.run(host='0.0.0.0', port=port)
     
     #app.run()
     #app.run(host='216.174.124.203',debug=True)
